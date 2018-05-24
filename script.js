6881e4ce55200249b7580e8865385cdf7b868bb1
Lawrence Archer <lawrence.archer@gmail.com>
2018-05-24 19:35:35 +0100

    finish all TOP requirements except pause & stop

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let duration = 0
 let workDisplay = document.querySelector("#workDisplay");
 let restDisplay = document.querySelector("#restDisplay");
 let start = document.querySelector("#start");
@@ -9,8 +10,24 @@ let workTimePlus = document.querySelector("#workTimePlus");
 let workTimeMinus = document.querySelector("#workTimeMinus");
 let restTimePlus = document.querySelector("#restTimePlus");
 let restTimeMinus = document.querySelector("#restTimeMinus");
+let pause = document.querySelector("#pause");
 start.addEventListener('click', (e) => {
-    timer(workTime.textContent*60, workDisplay);
+    duration = workTime.textContent*60;
+    timer(duration, workDisplay);
+    setTimeout(function(){
+        console.log(duration)
+    }, 5000);
+    setTimeout(function(){
+        duration = restTime.textContent*60;
+        timer(duration, restDisplay)
+    }, duration *1000);
+    if (duration < 0){
+        duration = restTime.textContent * 60;
+        timer (duration, restDisplay);
+    }
+});
+pause.addEventListener('click', (e) => {
+    //fix duration + stop timer function
 });
 reset.addEventListener('click', (e) => {
     location.reload();
@@ -42,9 +59,8 @@ function timer(duration, display) {
         display.textContent = minutes + ":" + seconds;
         duration --;
         
-        if (duration <= 0) {
+        if (duration < 0) {
             clearInterval(interval);
-            timer(restTime.textContent*60, restDisplay);
         }
     }, 1000);
 }
